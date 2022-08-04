3ce6a9cc244e3a814018c809851a9f1e9c57e522
Elizabeth Martín Campos <me@elizabeth.sh>
2022-08-04 23:17:18 +0200

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -9,12 +9,15 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 sudo chsh -s $(which zsh) $(whoami)
 
 # Zsh theme
-mv ./elizabeth.zsh-theme $HOME/.oh-my-zsh/custom/themes/elizabeth.zsh-theme
+cp ./elizabeth.zsh-theme $HOME/.oh-my-zsh/custom/themes/elizabeth.zsh-theme
 ## curl -fsSLo $HOME/.oh-my-zsh/custom/themes/elizabeth.zsh-theme https://raw.github.com/elizabeth-dev/dotfiles/main/elizabeth.zsh-theme
 
 # Zsh autosuggestions
 echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list
 curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
+
+# Zshrc
+cp ./.zshrc ~/.zshrc
 
 # Install packages
 sudo apt-get update && sudo apt-get install zsh-syntax-highlighting zsh-autosuggestions
